980a68ec70a034cf68f00a6b3e4ecbf953f0c951
Takahiro <p0o919961125@gmail.com>
2020-03-20 08:28:31 +0000

    third commit

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 15}
+  
+  validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  
+  validates :password, length: { minimum: 8, maximum: 32 }, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-zA-Z\d]+\z/i}
+  has_secure_password
+end
